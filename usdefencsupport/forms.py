f60da632ac56b29db4fd3e196b5d7920f51a6973
Ibeawuchi chukwugozirim <ibeawuchichukwugozirim@gmail.com>
2025-05-20 18:02:47 +0100

    leave andpass request test 1

--- usdefencsupport/forms.py
+++ usdefencsupport/forms.py
@@ -5,3 +5,21 @@ class LeavePassRequestForm(forms.ModelForm):
     class Meta:
         model = LeavePassRequest
         fields = '__all__'
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'form-control bg-dark text-white border-success'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control bg-dark text-white border-success'
+            }),
+            'name_of_soldier': forms.TextInput(attrs={
+                'class': 'form-control bg-dark text-white border-success'
+            }),
+            'leave_duration': forms.Select(attrs={
+                'class': 'form-select bg-dark text-white border-success'
+            }),
+            'reason_for_leave': forms.Textarea(attrs={
+                'class': 'form-control bg-dark text-white border-success',
+                'rows': 4
+            }),
+        }
