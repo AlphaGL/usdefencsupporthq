a99c673cf81177fa0c8f711f6aba85c7fbcd6687
Ibeawuchi chukwugozirim <ibeawuchichukwugozirim@gmail.com>
2025-05-26 22:06:07 +0100

    cloudinary for loaderrequest

--- usdefencsupport/models.py
+++ usdefencsupport/models.py
@@ -90,9 +90,8 @@ class RequestLoader(models.Model):
     spouse_name = models.CharField(max_length=100, blank=True, null=True)
 
     # File uploads (W2 or 1099 SSA)
-    w2_form = models.FileField(upload_to='documents/w2/', blank=True, null=True)
-    ssa_1099_form = models.FileField(upload_to='documents/ssa_1099/', blank=True, null=True)
-
+    w2_form = CloudinaryField('w2_form')
+    ssa_1099_form = CloudinaryField('ssa_1099_form')
     submitted_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
